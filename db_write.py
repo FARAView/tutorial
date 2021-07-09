3e2fd24f71b26d3b63573d1b3fd66a35e0697de2
Suyeol Yun <syyun@mit.edu>
2021-07-09 22:24:58 +0900

    add connection info & remove unnecessary lines

--- db_write.py
+++ db_write.py
@@ -10,28 +10,10 @@ del df['Unnamed: 7']
 del df['Unnamed: 8']
 del df['Unnamed: 9']
 
-
-def format_datetime(date_str):
-    if date_str:
-        datetime_format = "%m/%d/%Y"
-        datetime_obj = datetime.datetime.strptime(date_str, datetime_format)
-        return datetime_obj
-    else:
-        return date_str
-
-
-# convert date string to python default datetime obj
-df["Registration Date"] = df["Registration Date"].apply(lambda x: format_datetime(x))
-df["Termination Date"] = df["Termination Date"].apply(lambda x: format_datetime(x))
-
-# clean-out first/end whitespaces
-df["Zip"] = df["Zip"].apply(lambda x: x.strip() if x else x)
-df["Address 2"] = df["Address 2"].apply(lambda x: x.strip() if x else x)
-
 # write df on db
 from conn import username, password, host, port, database
 engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
-df.to_sql(name='registrants', con=engine, index=False, if_exists='replace')
+df.to_sql(name='exhibition_schedule_sy', con=engine, index=False, if_exists='replace')
 
 if __name__ == "__main__":
     pass
